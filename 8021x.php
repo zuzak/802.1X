208339cedea91744c3492163e9c2805afd7b9db4
Douglas Gardner <douglas@chippy.ch>
2012-11-14 12:52:58 +0000

    8021.x → 802.1x

--- 8021x.php
+++ 8021x.php
@@ -4,7 +4,7 @@ require "main.php";
 
 <html>
 	<head>
-		<title>8021.X</title>
+		<title>802.1X</title>
 		<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="guide.css">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
